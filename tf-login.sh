58397b5f8f7c8253acb912f1d14588ed6ad95e85
quyennguyen <quyennv@quyennv.com>
2024-09-18 21:21:25 +0700

    update login

--- tf-login.sh
+++
@@ -1,8 +0,0 @@
-TOKEN=${TF_GITHUB_ACTION_TOKEN}
-echo "the token ${TF_GITHUB_ACTION_TOKEN}"
-curl \
-  --header "Authorization: Bearer $TOKEN" \
-  --header "Content-Type: application/vnd.api+json" \
-  --request POST \
-  --data @payload.json \
-  https://app.terraform.io/api/v2/organizations/vti-practices-tf/authentication-token
