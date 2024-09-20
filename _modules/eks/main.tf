fad5dbf02ae56f4d0e94e2c06eb51bd1291fdd0b
quyennguyen <quyennv@quyennv.com>
2024-09-20 20:49:46 +0700

    move the create scaler

--- _modules/eks/main.tf
+++ _modules/eks/main.tf
@@ -189,69 +189,6 @@ module "eks_private_alb_security_group" {
 
   tags = var.tags
 }
-module "cluster_autoscaler_iam_assumable_role_admin" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.44.0"
-  create_role                   = true
-  role_name                     = "${var.eks_cluster_name}-eks-cluster-autoscaler"
-  provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
-  role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:cluster-autoscaler"]
-}
-
-# resource "aws_iam_policy" "cluster_autoscaler" {
-#   depends_on = [
-#     module.eks
-#   ]
-#   name_prefix = "${var.eks_cluser_enginee_version}-eks-cluster-autoscaler"
-#   description = "EKS cluster-autoscaler policy for cluster ${module.eks.cluster_id}"
-#   policy      = data.aws_iam_policy_document.cluster_autoscaler.json
-# }
-
-# data "aws_iam_policy_document" "cluster_autoscaler" {
-#   depends_on = [
-#     module.eks
-#   ]
-#   statement {
-#     sid    = "clusterAutoscalerAll"
-#     effect = "Allow"
-
-#     actions = [
-#       "autoscaling:DescribeAutoScalingGroups",
-#       "autoscaling:DescribeAutoScalingInstances",
-#       "autoscaling:DescribeLaunchConfigurations",
-#       "autoscaling:DescribeTags",
-#       "ec2:DescribeLaunchTemplateVersions",
-#     ]
-
-#     resources = ["*"]
-#   }
-
-#   statement {
-#     sid    = "clusterAutoscalerOwn"
-#     effect = "Allow"
-
-#     actions = [
-#       "autoscaling:SetDesiredCapacity",
-#       "autoscaling:TerminateInstanceInAutoScalingGroup",
-#       "autoscaling:UpdateAutoScalingGroup",
-#     ]
-
-#     resources = ["*"]
-
-#     condition {
-#       test     = "StringEquals"
-#       variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${module.eks.cluster_id}"
-#       values   = ["owned"]
-#     }
-
-#     condition {
-#       test     = "StringEquals"
-#       variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
-#       values   = ["true"]
-#     }
-#   }
-# }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
